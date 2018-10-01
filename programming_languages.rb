47f8c4863ce89f39d0d033d5ae2f77b65da34d0f
Alexander Truong <alexander.m.truong@gmail.com>
2018-10-01 20:34:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,47 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
  new_hash = {}
+ languages.each do |style, language|
+   language.each do |name, type_info|
+     if new_hash[name]
+       new_hash[name][:style] << style
+     else
+       new_hash[name] = {style: [style], type: type_info[:type]}
+     end
+   end
+ end
+ new_hash
+end
  
 end
